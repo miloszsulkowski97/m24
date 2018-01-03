d8efa67254136540be29a9884459b3059aa0f0f3
miloszsulkowski97 <milosz.sulkowski28@gmail.com>
2018-01-03 10:10:41 +0100

    Create isEmail.php

---
+++ isEmail.php
@@ -0,0 +1,7 @@
+class Validate extends ValidateCore {
+
+	public static function isEmail($email)
+	    {
+	        return !empty($email) && preg_match(Tools::cleanNonUnicodeSupport("/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"), $email);
+	    }
+}
